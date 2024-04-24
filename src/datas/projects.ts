3680e769d8b8cfbbb72419118ff48ff4764db1ed
AsheshRanaGurung <ashesh7272@gmail.com>
2024-04-24 10:49:09 +0545

    feat: added iframe

--- src/datas/projects.ts
+++ src/datas/projects.ts
@@ -1,65 +1,98 @@
-const myProjects=[
-    {
-        id:1,
-        title:"SRE Incident Mgmt Tool",
-        desc:"Automate incident response, reduce downtime and enhance your tech team's delivery with Squadcast’s Reliability Automation Platform. Bring together all your on-call and incident response workflows under one unified platform.",
-        image:"squadcast"
-        
-    },
-    {
-        id:2,
-        title:"Loan Mgmt Tool",
-        desc:"It is a dashboard Fin-tech product of MIFIN focused for loan retrieving from clients",
-        image:"qualtech"
+const myProjects = [
+  {
+    id: 1,
+    title: "SRE Incident Mgmt Tool",
+    desc: "Automate incident response, reduce downtime and enhance your tech team's delivery with Squadcast’s Reliability Automation Platform. Bring together all your on-call and incident response workflows under one unified platform.",
+    image: "squadcast",
+    link:"https://www.squadcast.com/",
+    iframeLink:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3889.0860354259257!2d77.67369087583351!3d12.902189116394798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1391f1666ee1%3A0x71ecc7aeac157b1f!2sSquadcast%20Labs%20Private%20limited!5e0!3m2!1sen!2snp!4v1713931764610!5m2!1sen!2snp",
+    tech: [
+      "TypeScript",
+      "Chakra UI",
+      "RTL",
+      "Zustand",
+      "React Hook Form",
+      "React Query",
+      "GraphQl",
+      "Zod",
+      "Lodash",
+      "React Error Boundary",
+      "Unleash Proxy Client",
+    ],
+    myContribution: [
+      "Squadcast is a product based end-to-end Incident management software based on US and India, helping SRE,DevOps and engineers do on-call scheduling and incident management in the SRE way.",
+      "2 weeks scrum plan where I get tickets on Jira board for buildingalerting and on-call management feature intuitive",
+      "Active oncall handover,meetings,standup with PM about feature . ",
+      "Common practise with Chakra-UI,sq own library,Ts,luxon,react hookform,react query,graphql to develop a feature. ",
+    ],
+  },
+  {
+    id: 2,
+    title: "Loan Mgmt Tool",
+    desc: "It is a dashboard Fin-tech product of MIFIN focused for loan retrieving from clients",
+    image: "qualtech",
+    link:"https://www.qualtechedge.com/",
+    iframeLink:"https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14010.277467094395!2d77.387651!3d28.612693!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cef8db73daae9%3A0x70c6f82448bac7a1!2sQualtech!5e0!3m2!1sen!2sin!4v1713933805514!5m2!1sen!2sin",
+    tech:[
+        "Vite",
+        "TypeScript",
+        "Chakra UI",
+        "Zustand",
+        "@tanstack",
+        "React Hook Form",
+        "React Query",
+        "Zod",
+        "React Error Boundary",
+    ],
+    myContribution:[
+        "Qualtech is a Fin-tech product of MIFIN based on India, focused for loan retrieving from clients.",
+        "Started project from scratch where tech like vite, react18,typescript, @tanstack, react-query ,zustand,react-hook-form ,Chakra was used.",
+        "Made reusable components for across the project under strict code review environment.",
+        "Worked as maintainer for PR reviews and quality code"
 
-    },
-    {
-        id:3,
-        title:"Trainee Mgmt System",
-        desc:"Web based dashboard system to manage, train ,schedule, exam, quiz resource, video learning platform  for government officers",
-        image:"sikai"
-
-    },
-    {
-        id:4,
-        title:"Doctor Appointment System",
-        desc:"Web based doctor appointment booking system including web video call and payment system",
-        image:"nepmeds"
-
-    },
-    {
-        id:5,
-        title:"Dharti Agro",
-        desc:"Website for indian client built using nextJS, shadcn ui and tailwind, reflecting their moto and campaign",
-        image:"dharti"
-
-    },
-    {
-        id:6,
-        title:"Moru blog page",
-        desc:"Moru is a fintech digital wallet company whose blog website was built using antD,redux-toolkit and vite",
-        image:"moru"
-    },
-    {
-        id:7,
-        title:"3D websites",
-        desc:"Built a website using ThreeJS and tailwind with interactive UI and working socials including emailJS",
-        image:"web"
-
-    },
-    {
-        id:8,
-        title:"this is SQ",
-        desc:"this is desc",
-        image:"dummy"
-
-    },
-    {
-        id:9,
-        title:"this is SQ",
-        desc:"this is desc",
-        image:"dummy"
-
-    },
-]
-export default myProjects
+    ]
+  },
+  {
+    id: 3,
+    title: "Trainee Mgmt System",
+    desc: "Web based dashboard system to manage, train ,schedule, exam, quiz resource, video learning platform  for government officers",
+    image: "sikai",
+  },
+  {
+    id: 4,
+    title: "Doctor Appointment System",
+    desc: "Web based doctor appointment booking system including web video call and payment system",
+    image: "nepmeds",
+  },
+  {
+    id: 5,
+    title: "Dharti Agro",
+    desc: "Website for indian client built using nextJS, shadcn ui and tailwind, reflecting their moto and campaign",
+    image: "dharti",
+  },
+  {
+    id: 6,
+    title: "Moru blog page",
+    desc: "Moru is a fintech digital wallet company whose blog website was built using antD,redux-toolkit and vite",
+    image: "moru",
+  },
+  {
+    id: 7,
+    title: "3D websites",
+    desc: "Built a website using ThreeJS and tailwind with interactive UI and working socials including emailJS",
+    image: "web",
+  },
+  {
+    id: 8,
+    title: "this is SQ",
+    desc: "this is desc",
+    image: "dummy",
+  },
+  {
+    id: 9,
+    title: "this is SQ",
+    desc: "this is desc",
+    image: "dummy",
+  },
+];
+export default myProjects;
--- src/pages/ProjectDetailPage.tsx
+++ src/pages/ProjectDetailPage.tsx
@@ -1,6 +1,5 @@
-import React from "react";
 import myProjects from "../datas/projects";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {
   squadcast,
   qualtech,
@@ -11,9 +10,11 @@ import {
   web,
   dummy,
 } from "../assets/images";
+import { PORTFOLIO_ROUTES } from "../../routes/routeConstant";
 
 const ProjectDetailPage = () => {
-  const { id } = useParams();
+  const { id = "" } = useParams();
+  const navigate = useNavigate();
 
   const projectImage: { [key: string]: string } = {
     squadcast: squadcast,
@@ -25,28 +26,60 @@ const ProjectDetailPage = () => {
     web: web,
     dummy: dummy,
   };
+  const individualProject =
+    myProjects.filter((proj) => proj.id === +id)[0] ?? [];
 
   return (
     <section className="relative w-full h-screen">
-      <div className=" gap-6 mx-[10%] mt-24  flex  sm:justify-center md:justify-start">
-     
-       <div className="flex-[0.7]">
-       {myProjects
-          .filter((proj) => proj.id === +id)
-          .map((proj) => (
-            <div>
-              <h1 className="subhead-text mb-4">{proj.title}</h1>
-              <div className="w-[50dvw]">
-
-              <img src={projectImage[proj.image]} alt="image" className="w-[100%] h-[100%]" />
-                </div>
+      <div className=" flex gap-14 mx-[10%] mt-24   sm:flex-col md:flex-row sm:justify-center md:justify-start">
+        <div className="flex-[0.7]">
+          <div>
+            <div className="flex">
+              <h1
+                className="subhead-text mb-4 cursor-pointer"
+                onClick={() => navigate(PORTFOLIO_ROUTES.PROJECT)}
+              >
+                Projects &gt;
+              </h1>
+              <h1 className="subhead-text mb-4">{individualProject.title}</h1>
             </div>
-          ))}
-       </div>
-       <div className="flex-[0.3]">
-       <h1 className="subhead-text mb-4">Tech Stack Used</h1>
-        
-</div>
+            <div className="w-[45dvw] flex flex-col gap-y-8">
+              <img
+                src={projectImage[individualProject.image]}
+                alt="image"
+                className="w-[100%] h-[100%]"
+              />
+              <div className=" flex flex-col gap-4">
+                {individualProject.myContribution?.map((tech) => (
+                  <p key={tech}>
+                    <li>{tech}</li>
+                  </p>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex-[0.3]">
+          <h1 className="subhead-text mb-4">Some Highlighted Tech Used</h1>
+          <div className="mb-4">
+            {individualProject.tech?.map((tech) => (
+              <li>{tech}</li>
+            ))}
+          </div>
+          <h1 className="subhead-text ">Want To know more about the client?</h1>
+          <div className="mb-4">
+            <a style={{ color: "blue" }} href={individualProject.link}>
+              Click here
+            </a>
+          </div>
+          <iframe
+            src={individualProject.iframeLink}
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            loading="lazy"
+          ></iframe>
+        </div>
       </div>
     </section>
   );
